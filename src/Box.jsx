7d9abd20d98cf9c2ae533fc80dd14f34404a2c21
Manuel Adrian Chavez Sanchez <manuel.adrian.chavez@ibm.com>
2024-11-06 09:29:33 -0600

    useRef

--- src/Box.jsx
+++ src/Box.jsx
@@ -1,6 +1,9 @@
+import { useRef } from 'react';
 export default function Box({ position, name, wireframe }) {
+  const ref = useRef();
+  console.log(ref);
   return (
-    <mesh position={position} name={name}>
+    <mesh position={position} name={name} ref={ref}>
       <boxGeometry />
       <meshBasicMaterial color={0x00ff00} wireframe={wireframe} />
     </mesh>
