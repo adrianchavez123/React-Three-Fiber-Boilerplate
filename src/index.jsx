064e6769adeb8eb01227b3b6ba2ad0cde81197eb
Manuel Adrian Chavez Sanchez <manuel.adrian.chavez@ibm.com>
2024-11-06 09:16:15 -0600

    components

--- src/index.jsx
+++ src/index.jsx
@@ -1,20 +1,11 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import { Canvas } from '@react-three/fiber'
-import './styles.css'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './App';
 
-function App() {
-  return (
-    <mesh>
-      <boxGeometry />
-      <meshBasicMaterial color={0x00ff00} wireframe />
-    </mesh>
-  )
-}
+import './styles.css';
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Canvas camera={{ position: [0, 0, 2] }}>
-      <App />
-    </Canvas>
+    <App />
   </StrictMode>
-)
+);
