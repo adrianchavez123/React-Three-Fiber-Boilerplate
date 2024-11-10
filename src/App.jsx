fa4202b86f250dee02ed675e8c8e3131454d447b
Manuel Adrian Chavez Sanchez <manuel.adrian.chavez@ibm.com>
2024-11-10 12:49:05 -0600

    env

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,18 @@
-import { Stats, OrbitControls, Circle } from '@react-three/drei';
+import { Stats, OrbitControls, Sphere, Environment } from '@react-three/drei';
 import { Canvas, useLoader } from '@react-three/fiber';
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
 
 export default function App() {
   const gltf = useLoader(GLTFLoader, '/models/monkey.glb');
 
-  console.log(gltf);
   return (
-    <Canvas camera={{ position: [-0.5, 1, 2] }} shadows>
-      <directionalLight
-        position={[3.3, 1.0, 4.4]}
-        castShadow
-        intensity={Math.PI * 2}
-      />
-      <primitive
-        object={gltf.scene}
-        position={[0, 1, 0]}
-        children-0-castShadow
-      />
-      <Circle args={[10]} rotation-x={-Math.PI / 2} receiveShadow>
-        <meshStandardMaterial />
-      </Circle>
+    <Canvas camera={{ position: [-0.5, 1, 2] }}>
+      <Environment preset="sunset" background blur={0.5} />
+      <directionalLight position={[3.3, 1.0, 4.4]} intensity={4}>
+        <Sphere args={[3.3, 1.0, 4.4]}></Sphere>
+      </directionalLight>
+      <primitive object={gltf.scene} position={[0, 1, 0]} />
+
       <OrbitControls target={[0, 1, 0]} />
       <axesHelper args={[5]} />
       <Stats />
