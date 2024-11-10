588297433534d24376119a6ccbe5feb0105d33fe
Manuel Adrian Chavez Sanchez <manuel.adrian.chavez@ibm.com>
2024-11-10 12:52:07 -0600

    fix blur

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { Stats, OrbitControls, Sphere, Environment } from '@react-three/drei';
+import { Stats, OrbitControls, Environment } from '@react-three/drei';
 import { Canvas, useLoader } from '@react-three/fiber';
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
 
@@ -7,10 +7,10 @@ export default function App() {
 
   return (
     <Canvas camera={{ position: [-0.5, 1, 2] }}>
-      <Environment preset="sunset" background blur={0.5} />
-      <directionalLight position={[3.3, 1.0, 4.4]} intensity={4}>
-        <Sphere args={[3.3, 1.0, 4.4]}></Sphere>
-      </directionalLight>
+      <Environment preset="sunset" background backgroundBlurriness={0.5} />
+      <directionalLight
+        position={[3.3, 1.0, 4.4]}
+        intensity={4}></directionalLight>
       <primitive object={gltf.scene} position={[0, 1, 0]} />
 
       <OrbitControls target={[0, 1, 0]} />
