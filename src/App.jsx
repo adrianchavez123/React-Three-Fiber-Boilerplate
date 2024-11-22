98d0a7976fcb1f21639912abb4dc676cd6dbd520
Manuel Adrian Chavez Sanchez <manuel.adrian.chavez@ibm.com>
2024-11-22 12:52:41 -0600

    hook

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,16 @@
-import { Canvas, useThree, useFrame } from '@react-three/fiber';
-import { Vector3 } from 'three';
-import { Stats, Environment, Center } from '@react-three/drei';
-import Button from './Button';
-
-const vec = new Vector3();
-
-function Rig() {
-  return useFrame(({ camera, pointer }) => {
-    vec.set(pointer.x * 2, pointer.y * 2, camera.position.z);
-    camera.position.lerp(vec, 0.025);
-    camera.lookAt(0, 0, 0);
-    console.log(camera.position.x); //Math damp "epsilon"
-  });
-}
+import { Canvas } from '@react-three/fiber';
+import Box from './Box';
+import { Stats, OrbitControls } from '@react-three/drei';
 
 export default function App() {
   return (
-    <Canvas camera={{ position: [0, 0, 5] }}>
-      <Environment preset="forest" background />
-      <Center>
-        {[...Array(5).keys()].map((x) =>
-          [...Array(3).keys()].map((y) => (
-            <Button key={x + y * 5} position={[x * 2.5, y * 2.5, 0]} />
-          ))
-        )}
-      </Center>
-      <Rig />
+    <Canvas camera={{ position: [1, 2, 3] }}>
+      <Box position={[-1.5, 0.5, 0]} selected />
+      <Box position={[0, 0.5, 0]} />
+      <Box position={[1.5, 0.5, 0]} />
+      <OrbitControls />
+      <axesHelper args={[5]} />
+      <gridHelper />
       <Stats />
     </Canvas>
   );
